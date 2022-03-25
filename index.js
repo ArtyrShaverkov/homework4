ab34694bf578dc661f0d3e5fb8e7cea580f989ef
ArtyrShaverkov <tyrusche@mail.ru>
2022-03-26 00:52:53 +0300

    homework4

---
+++ index.js
@@ -0,0 +1,30 @@
+// summa
+function summaExs(a,b){
+    return a+b;
+}
+const summaDec=function (a,b){
+    return a+b;
+}
+const summaArr = (a,b) => {
+  return a+b;
+}
+console.log(`summa:${summaExs(3,9)}`)
+// difference
+
+// multiply
+
+// division
+
+// modulo
+
+//more
+
+//less
+
+//more exactly
+
+// less exactly
+
+// exactly
+
+ // notexactly
